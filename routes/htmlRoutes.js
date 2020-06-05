4ce33800b5e34827513aa8ea26730926083ae81c
Tyler Buck <57814333+thetylerb@users.noreply.github.com>
2020-06-05 18:06:24 -0400

    fix routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,12 @@
-
 var path = require("path");
 
+module.exports = function(app){
 
-module.exports = function(app) {
+  app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+  })
 
-
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../note-taker/public/index.html"));
-  });
-
-  app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../note-taker/public/notes.html"));
-  });
-
-  // If no matching route is found default to home
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../note-taker/public/index.html"));
-  });
-};
+  app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+  })
+}
